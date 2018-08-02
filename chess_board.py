a93a65ae52863c6f1574f86a4bccef7a407b223a
zzzzzzli <zhanglilee1992@outlook.com>
2018-08-02 19:13:18 +0800

    can delete piece but no judgement whether a player is deletable

--- chess_board.py
+++ chess_board.py
@@ -37,6 +37,7 @@ def addPiece(player,center):
     user = getPiecePlayer(color)
     piece = Piece(cir,color,getPieceLocation(center),user)
     pieces[user].append(piece)
+    return piece
 
 def getLocationFromMouse(mouse):
     l = []
@@ -92,7 +93,7 @@ def getPieceFromLocation(loc):
         for piece in pieces[user]:
          #   print(str(piece.location)+', '+str(loc))
             if piece.location == loc:
-                print(pieces[user].index(piece))
+               # print(pieces[user].index(piece))
                 return piece
     return None
 
@@ -117,22 +118,22 @@ def isInSquare(piece):
             s = user
             for i in ifsquare:
                 if neb[i]==None:
-                    return False,False
+                    return 0,0
                 else:
                     s += neb[i].user
-                    print("s="+str(s/4))
-            return bool(s/4==user),False
+                #    print("s="+str(s/4))
+            return int(bool(s/4==user)),0
         elif y==size-1:
         # 左下角
             ifsquare = [0,5,2]
             s = user
             for i in ifsquare:
                 if neb[i]==None:
-                   return False,False
+                   return 0,0
                 else:
                     s += neb[i].user
-                    print("s="+str(s))
-            return bool(s/4==user),False
+                  #  print("s="+str(s))
+            return int(bool(s/4==user)),0
 
         else:
         # 左边
@@ -143,13 +144,13 @@ def isInSquare(piece):
                 s2 += 5 if neb[ifsquare[-i-1]]==None else neb[ifsquare[-(i+1)]].user
             s1 = s1/4
             s2 = s2/4
-            print("s1="+str(s1)+",s2="+str(s2))
+          #  print("s1="+str(s1)+",s2="+str(s2))
             if s1==user and s2==user:
-                return True,True
+                return 1,1
             elif s1==user or s2==user:
-                return True,False
+                return 1,0
             else:
-                return False,False
+                return 0,0
     elif x==size-1:
         if y==0:
         # 右上角
@@ -157,22 +158,22 @@ def isInSquare(piece):
             s = user
             for i in ifsquare:
                 if neb[i]==None:
-                    return False,False
+                    return 0,0
                 else:
                     s += neb[i].user
-            print("s="+str(s))
-            return bool(s/4==user),False
+         #   print("s="+str(s))
+            return int(bool(s/4==user)),0
         elif y==size-1:
         # 右下角
             ifsquare = [0,4,3]
             s = user
             for i in ifsquare:
                 if neb[i]==None:
-                    return False,False
+                    return 0,0
                 else:
                     s += neb[i].user
-                print("s="+str(s))
-            return bool(s/4==user),False
+             #   print("s="+str(s))
+            return int(bool(s/4==user)),0
 
         else:
         # 右边
@@ -183,13 +184,13 @@ def isInSquare(piece):
                 s2 += 5 if neb[ifsquare[i+2]]==None else neb[ifsquare[i+2]].user
             s1 = s1/4
             s2 = s2/4
-            print("s1="+str(s1)+",s2="+str(s2))
+          #  print("s1="+str(s1)+",s2="+str(s2))
             if s1==user and s2==user:
-                return True,True
+                return 1,1
             elif s1==user or s2==user:
-                return True,False
+                return 1,0
             else:
-                return False,False
+                return 0,0
     else:
         if y==0:
         # 上边
@@ -200,13 +201,13 @@ def isInSquare(piece):
                 s2 += 5 if neb[ifsquare[i+2]]==None else neb[ifsquare[i+2]].user
             s1 = s1/4
             s2 = s2/4
-            print("s1="+str(s1)+",s2="+str(s2))
+         #   print("s1="+str(s1)+",s2="+str(s2))
             if s1==user and s2==user:
-                return True,True
+                return 1,1
             elif s1==user or s2==user:
-                return True,False
+                return 1,0
             else:
-                return False,False
+                return 0,0
         elif y==size-1:
         # 下边
             ifsquare = [3,4,0,5,2]
@@ -216,13 +217,13 @@ def isInSquare(piece):
                 s2 += 5 if neb[ifsquare[-i-1]]==None else neb[ifsquare[-(i+1)]].user
             s1 = s1/4
             s2 = s2/4
-            print("s1="+str(s1)+",s2="+str(s2))
+         #   print("s1="+str(s1)+",s2="+str(s2))
             if s1==user and s2==user:
-                return True,True
+                return 1,1
             elif s1==user or s2==user:
-                return True,False
+                return 1,0
             else:
-                return False,False
+                return 0,0
         else:
             ifsquare = [0,4,3,6,1,7,2,5,0]
             s1,s2,s3,s4 = user,user,user,user
@@ -235,18 +236,57 @@ def isInSquare(piece):
             s2 = s2/4
             s3 = s3/4
             s4 = s4/4
-            print("s1="+str(s1)+",s2="+str(s2)+",s3="+str(s3)+",s4="+str(s4))
+        #    print("s1="+str(s1)+",s2="+str(s2)+",s3="+str(s3)+",s4="+str(s4))
             if (s1==user and s2==user) or (s2==user and s3==user) or (s3==user and s4==user) or (s4==user and s1==user):
-                return True,True
+                return 1,1
             elif s1==user or s2==user or s3==user or s4==user:
-                return True,False
+                return 1,0
             else:
-                return False,False
+                return 0,0
+
+def isAline(piece):
+    x,y = piece.location
+    for i in range(10):
+        l1 = (i,y)
+        l2 = (x,i)
+        for l in [l1,l2]:
+            p = getPieceFromLocation(l)
+            if p==None:
+                return 0
+            elif p.user!=piece.user:
+                return 0
+            else:
+                pass
+    return 1
+
+def deletePiece(piece):
+    piece.circle.undraw()
+    u = piece.user
+    i=0
+    for x in pieces[u]:
+        if x.location==piece.location:
+            del pieces[u][i]
+            return
+        i += 1
+            
+def selectPieceToDelete(user):
+    loc = getPieceLocation(getLocationFromMouse(win.getMouse()))
+    print(loc)
+    p = getPieceFromLocation(loc)
+    while p == None or bool(sum(isInSquare(p))) or p.user==user:
+        print("an invalid piece to delete, please select another")
+        print(p)
+        if p!=None:
+            print(str(p.user)+','+str(user))
+            print(isInSquare(p))
+        loc = getPieceLocation(getLocationFromMouse(win.getMouse()))
+        p = getPieceFromLocation(loc)
+    return p
 
 
 pts = 30
-size = 10
-piece_radius = pts//4
+size = 6
+piece_radius = 3*pts//8
 players={0:'black',1:'white'}
 win = chessBoard(size)
 pieces = [[],[]]
@@ -256,10 +296,21 @@ while True:
         mouse = win.getMouse()
         while not isLocationValid(mouse):
             mouse = win.getMouse()
-        addPiece(user,getLocationFromMouse(mouse))
-        print('location: '+str(pieces[user][-1].location)+', player: '+str(user))
-        print(str(isInSquare(pieces[user][-1])))
+        piece = addPiece(user,getLocationFromMouse(mouse))
+      #  print('location: '+str(piece.location)+', player: '+str(user))
+      #  print(str(isInSquare(piece)))
+        n = sum(isInSquare(piece))+isAline(piece)
+#        for i in range(n):
+#            print("Plise select "+str(n)+" pieces of opponent to delete")
+#            p = selectPieceToDelete(user)
+#            deletePiece(p)
+        
+                
     if len(pieces[0])+len(pieces[1])==size**2:
+        for a in range(size**2):
+            for u in range(2):
+                print(u)
+                deletePiece(selectPieceToDelete(u))
         break
 win.getMouse()
 
